31493a0b178d4ad6c6fee42927c7ec0341bf7915
juhyung1 <whans1996@naver.com>
2026-09-07 17:15:00 +0900

    0907 싸피

---
+++ Trail4_Intermediate_Low/src/Chapter1_Simulation/기울어진직사각형.java
@@ -0,0 +1,33 @@
+package Chapter1_Simulation;
+
+import java.util.Scanner;
+
+public class 기울어진직사각형 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[][] arr = new int[N][N];
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}	// main
+}
---
+++ Trail4_Intermediate_Low/src/Chapter1_Simulation/안겹치는_두_직사각형.java
@@ -0,0 +1,10 @@
+package Chapter1_Simulation;
+
+public class 안겹치는_두_직사각형 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
